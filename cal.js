ec6c656f916823db73297a3b2f2d21c92c963b29
chenys1995 <argorok7@gmail.com>
2016-11-08 14:26:13 +0800

    infix to postfix evaluation complete

--- cal.js
+++ cal.js
@@ -13,24 +13,114 @@ function infix_expr(op){
 				reload();
 				return;
 			}
-			console.log(top)
-			top = parseInt(top,10)*10;
-			console.log(top)
-			top = parseInt(top,10) + parseInt(op,10);
-			console.log(top)
+			top += op;
 		}
 		else top = op;
 		expr.push(top);
 	}
-	console.log(expr)
+	//console.log(expr)
 	reload();
 }
-
+ function InfixtoPostfix(){
+	var pfixString = [];
+	var infixStack = [];
+	
+	// Helper function to get the precedence of the operator
+	var precedence = function(operator){
+		switch(operator){
+		case "*":
+		case "/":
+		case "%":
+			return 2;
+		case "+":
+		case "-":
+			return 1;
+		default:
+			return 0;
+		}
+	}
+	var l = expr.length;
+	for(var i=0; i<l; i++){
+		var c = expr[i];
+		if(!isNaN(parseInt(c))){
+			pfixString.push(c);
+		}else if(c === "+" || c==="-" || c === "*" || c==="/" || c==="%"){
+			while(!isEmpty(infixStack) && (precedence(c) <= precedence(get_top(infixStack)))){
+				pfixString.push(infixStack.pop());
+			}
+			infixStack.push(c);
+		}
+	}
+	
+	while(!isEmpty(infixStack)){
+		pfixString.push(infixStack.pop());
+	}
+	return pfixString;
+}
+function isEmpty(stack){
+	return stack.length ===0
+}
+function get_top(stack){
+	var top = stack.pop();
+	stack.push(top);
+	return top;
+}
+function eval(){
+	var postfix = InfixtoPostfix();
+	 console.log("postfix :")
+	console.log(postfix)
+	var stack =[];
+	for(var i in postfix){
+		 switch(postfix[i]){
+			 case "+":
+				var op2 = stack.pop(),op1 = stack.pop();
+				stack.push(parseInt(op1,10)+parseInt(op2,10));
+				break;
+			 case "-":
+				var op2 = stack.pop(),op1 = stack.pop();
+				stack.push(parseInt(op1,10)-parseInt(op2,10));
+				break;
+			 case "*":
+				var op2 = stack.pop(),op1 = stack.pop();
+				stack.push(parseInt(op1,10)*parseInt(op2,10));
+				break;
+			 case "/":
+				var op2 = stack.pop(),op1 = stack.pop();
+				if(parseInt(op2,10) != 0)
+					stack.push(parseInt(op1,10)/parseInt(op2,10));
+				else stack.push(NaN);
+				break;
+			 case "%":
+				var op2 = stack.pop(),op1 = stack.pop();
+				if(parseInt(op2,10) != 0)
+					stack.push(parseInt(op1,10)%parseInt(op2,10));
+				else stack.push(NaN);
+				break;
+			 default:
+				stack.push(postfix[i]);
+				break;
+		 }
+		 console.log(stack)
+	}
+	display(get_top(stack));
+	reload();
+}
 function sign_change(){
 	var top = expr.pop();
 	top *= -1;
 	expr.push(top);
 	reload();
+}
+function display(ans){
+	var result_hex = document.getElementById("result_hex");
+	var result_dec = document.getElementById("result_dec");
+	var result_oct = document.getElementById("result_oct");
+	var result_bin = document.getElementById("result_bin");
+	var hex = ans.toString(16),oct =ans.toString(8),bin=ans.toString(2);
+	result_hex.innerHTML = "HEX "+hex;
+	result_dec.innerHTML = "DEC " +parseInt(hex,16);
+	result_oct.innerHTML = "OCT " +oct;
+	result_bin.innerHTML = "BIN " +bin;
 }
 function reload(){
 	var result = document.getElementById("result");
