4526d067e8f8159d52d6892744559575510833f0
chenys1995 <argorok7@gmail.com>
2017-01-04 15:57:19 +0800

    add trigger test

--- tests/test1.js
+++ tests/test1.js
@@ -1,3 +1,35 @@
+QUnit.module("groupA");
+function init(){
+	let calc = new Calc();
+  render({calc});
+  $('.button').on('click', function(e) {
+    if ($(this).hasClass('disabled')) return;
+    let content = $(this).data('content');
+    calc.exec(content)
+      .then(() => {
+        render({calc});
+      })
+      .catch(err => console.log(err));
+  });
+  $('.base').on('click', function(e) {
+    let content = $(this).data('content');
+    calc.base = content|0;
+    calc.exec('')
+      .then(() => {
+        render({calc});
+      })
+      .catch(err => console.log(err));
+  });
+
+  keyBinging.forEach(({key, content}) => {
+    Mousetrap.bind(key, () => $(`.button[data-content=${content}]`).click());
+    $(`.button[data-content=${content}]`).attr('title', `[${key}]`);
+  })
+  Mousetrap.bind('h e x', () => $(`.base.hex`).click());
+  Mousetrap.bind('d e c', () => $(`.base.dec`).click());
+  Mousetrap.bind('o c t', () => $(`.base.oct`).click());
+  Mousetrap.bind('b i n', () => $(`.base.bin`).click());
+}
 QUnit.test("isVal_test", function(assert)
 {
     assert.deepEqual(isVal(["val"]), true);
@@ -25,4 +57,18 @@ QUnit.test("arithmetric_test", function(assert)
 	assert.deepEqual(evaluate(tcs), 7);
 	tcs = [["val","4"],["oper","div"],["val","2"],["oper","mul"],["mul","3"],["oper","add"],["val","5"]];
 	assert.deepEqual(evaluate(tcs), 11);
+});
+QUnit.test("Trigger_test", function(assert)
+{
+	init();
+	$(`.button[data-content=${'val-1'}]`).trigger("click");
+	$(`.button[data-content=${'oper-add'}]`).trigger("click");
+	$(`.button[data-content=${'val-2'}]`).trigger("click");
+	$(`.button[data-content=${'calc'}]`).trigger("click");
+	var done = assert.async();
+	var input = $('.value').focus();
+	setTimeout(function() {
+	assert.equal(input[0].innerHTML,"3");
+	done();
+	}, 1000);
 });
